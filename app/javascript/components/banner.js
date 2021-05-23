04c2b5f0970f6f4f709a262713795c6d0d5d34d7
Noemi Ashizuka <noemi.calanducci@gmail.com>
2021-05-23 23:30:55 +0900

    fixed some javascript error in console

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,14 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["Moon Prism Power Make Up!", "Create your Sailor Warrior"],
-    typeSpeed: 50,
-    loop: true
-  });
+  const tag = document.querySelector('#banner-typed-text')
+  if(tag) {
+    new Typed('#banner-typed-text', {
+      strings: ["Moon Prism Power Make Up!", "Create your Sailor Warrior"],
+      typeSpeed: 50,
+      loop: true
+    });
+  }
 }
 
 export { loadDynamicBannerText };
